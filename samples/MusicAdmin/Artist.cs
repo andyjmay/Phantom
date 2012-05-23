d95d55d20733656ca72d807350e9f45937896a2b
Andy May <andyjmay@gmail.com>
2012-05-22 20:33:15 -0400

    Initial skeleton for sample projects
    This branch will be used for any sample apps that are worked on before
    being committed to master.

---
+++ samples/MusicAdmin/Artist.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using Phantom;
+
+namespace MusicAdmin {
+  [Export(typeof(ModelBase))]
+  public class Artist {
+    public string Name { get; set; }
+
+  }
+}
