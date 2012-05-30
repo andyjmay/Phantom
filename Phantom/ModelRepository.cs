ae82717d4d739389efe9bdef6d14eae7c0289f58
Andy May <andyjmay@gmail.com>
2012-05-30 09:15:00 -0400

    Change generic parameter
    This commit makes the generic type be on the Model itself instead of on
    the methods. I think this make more sense.

--- Phantom/ModelRepository.cs
+++ Phantom/ModelRepository.cs
@@ -2,32 +2,32 @@
 using System.Linq;
 
 namespace Phantom {
-  public class ModelRepository : IModelRepository {
+  public class ModelRepository<T> : IModelRepository<T> where T : ModelBase {
     private readonly ModelContext db;
 
     public ModelRepository(ModelContext dataContext) {
       db = dataContext;
     }
 
-    public IQueryable<T> GetAll<T>() where T : ModelBase {
+    public IQueryable<T> GetAll() {
       return db.Set<T>().OfType<T>();
     }
 
-    public T GetById<T>(int modelID) where T : ModelBase {
+    public T GetById(int modelID) {
       return db.Set<T>().OfType<T>().Single(m => m.ID == modelID);
     }
 
-    public T Add<T>(T modelToAdd) where T : ModelBase {
+    public T Add(T modelToAdd) {
       db.Set<T>().Add(modelToAdd);
       return modelToAdd;
     }
 
-    public T Update<T>(T modelToUpdate) where T : ModelBase {
+    public T Update(T modelToUpdate) {
       db.Entry(modelToUpdate).State = EntityState.Modified;
       return modelToUpdate;
     }
 
-    public void Delete<T>(T modelToUpdate) where T : ModelBase {
+    public void Delete(T modelToUpdate) {
       db.Entry(modelToUpdate).State = EntityState.Deleted;
     }
 
